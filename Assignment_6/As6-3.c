58c4295c5cac07ab2cb71b9b5744b12d0988f52f
Rahulj-0707 <jadhavrahul1824@gmail.com>
2025-05-17 23:13:02 +0530

    Add files via upload

---
+++ Assignment_6/As6-3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int Factorial(int iNo)
+{
+    int iCnt = 0;
+    int iFact = 1;
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        iFact *= iCnt;
+    }
+    return iFact;
+
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    printf("Enter number");
+    scanf("%d",&iValue);
+    iRet = Factorial(iValue);
+    printf("%d",iRet);
+
+    return 0;
+
+}
