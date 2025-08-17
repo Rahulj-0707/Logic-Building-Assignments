1fcfcf60e4df3f9c1cf344dade7dea18824df173
Rahulj-0707 <jadhavrahul1824@gmail.com>
2025-08-18 00:40:47 +0530

    Add files via upload

---
+++ Assignment_18/As18-5.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+void Display(int Arr[], int iLength)
+{
+    int iCnt = 0;
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if((Arr[iCnt] % 11) == 0)
+        {
+            printf("Number is Multiple of 11 %d\n",Arr[iCnt]);
+        }
+    }
+}
+int main()
+{
+    int iSize = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter the number of elements:");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory:");
+        return -1;
+    }
+    printf("Enter %d element\n",iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter elements : %d:",iCnt);
+        scanf("%d",&p[iCnt]);
+    }
+
+    Display(p, iSize);
+
+
+    free(p);
+
+    return 0;
+}
