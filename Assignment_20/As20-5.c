789138b82cd1ed20e6d7bad38f45a6f0c8c41707
Rahulj-0707 <jadhavrahul1824@gmail.com>
2025-08-18 22:36:36 +0530

    Add files via upload

---
+++ Assignment_20/As20-5.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Product(int Arr[], int iLength)
+{
+    int iCnt = 0, iProduct = 1;
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if((Arr[iCnt] % 2) != 0)
+        {
+            iProduct = iProduct * Arr[iCnt];
+        }
+    }
+    return iProduct;
+
+}
+int main()
+{
+    int iSize = 0, iCnt = 0, iRet = 0;
+    int *p = NULL;
+
+    printf("Enter the number of elements :\n");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate the memory");
+        return -1;
+    }
+
+    printf("Enter %d element\n",iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt);
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Product(p, iSize);
+    printf("Product is %d",iRet);
+
+    free(p);
+
+    return 0;
+
+}
